72a205a0ee6f8abba7e1ba0f14c9d81a097c307f
Lukas Lazarek <LukasALazarek@gmail.com>
2018-09-19 11:44:45 -0500

    Add contracts to un-types

--- benchmarks/dungeon/base/un-types.rkt
+++ benchmarks/dungeon/base/un-types.rkt
@@ -1,14 +1,20 @@
 #lang racket/base
 
+(require racket/contract)
+
 (provide
   index?
   assert
 )
 
-(define (assert v p)
+(define/contract (assert v p)
+  (parametric->/c [A] (A (A . -> . boolean?) . -> . A))
+
   (if (p v)
     v
     (raise-user-error 'assert)))
 
-(define (index? v)
+(define/contract (index? v)
+  (any/c . -> . boolean?)
+
   (and (exact-nonnegative-integer? v) (< v 9999999)))
