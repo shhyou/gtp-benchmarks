e7b7fea50969c6b8e1db886f3789abeae95d5723
Lukas Lazarek <LukasALazarek@gmail.com>
2018-09-20 15:40:15 -0500

    dungeon: refine message-queue contracts

--- benchmarks/dungeon/untyped/message-queue.rkt
+++ benchmarks/dungeon/untyped/message-queue.rkt
@@ -1,6 +1,7 @@
 #lang racket/base
 
-(require racket/contract)
+(require racket/contract
+         (only-in racket/list first empty?))
 
 (provide
   enqueue-message!
@@ -15,11 +16,19 @@
   '())
 
 (define/contract (enqueue-message! m)
-  (string? . -> . void?)
+  (let ([pre/queue-len #f])
+    (->i ([m string?])
+         #:pre () (set! pre/queue-len (length message-queue))
+         [result void?]
+         #:post (m) (and (string=? m (first message-queue))
+                         (= (length message-queue)
+                            (add1 pre/queue-len)))))
 
   (set! message-queue (cons m message-queue)))
 
 (define/contract (reset-message-queue!)
-  (-> void?)
+  (->* () ()
+       void?
+       #:post (empty? message-queue))
 
   (set! message-queue '()))
