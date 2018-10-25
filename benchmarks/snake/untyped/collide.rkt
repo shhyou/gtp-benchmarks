c125a794b88a226c1e25f20b3aa51cfb484effe5
Lukas Lazarek <LukasALazarek@gmail.com>
2018-10-25 12:55:42 -0500

    snake: start contracting; ctcs for collide

--- benchmarks/snake/untyped/collide.rkt
+++ benchmarks/snake/untyped/collide.rkt
@@ -1,26 +1,62 @@
-#lang racket  
+#lang racket
 (require "data.rkt"
-         "const.rkt")
+         "const.rkt"
+         "../../../ctcs/precision-config.rkt"
+         "../../../ctcs/common.rkt"
+         (only-in racket/function curry))
+
 
 ;; snake-wall-collide? : Snake -> Boolean
 ;; Is the snake colliding with any of the walls?
-(define (snake-wall-collide? snk)
+(define/contract (snake-wall-collide? snk)
+  (configurable-ctc
+   [max (->i ([snk snake?])
+             [result (snk)
+                     (head-collide? (car (snake-segs snk)))])]
+   [types (snake? . -> . boolean?)])
+
   (head-collide? (car (snake-segs snk))))
 
 ;; head-collide? : Posn -> Boolean
-(define (head-collide? p)
+(define/contract (head-collide? p)
+  (configurable-ctc
+   [max (->i ([p posn?])
+             [result (p)
+                     (not (and (<= 0 (posn-x p) BOARD-WIDTH)
+                               (<= 0 (posn-y p) BOARD-HEIGHT)))])]
+   [types (posn? . -> . boolean?)])
+
   (or (<= (posn-x p) 0)
       (>= (posn-x p) BOARD-WIDTH)
       (<= (posn-y p) 0)
       (>= (posn-y p) BOARD-HEIGHT)))
 
 ;; snake-self-collide? : Snake -> Boolean
-(define (snake-self-collide? snk)
+(define/contract (snake-self-collide? snk)
+  (configurable-ctc
+   [max (->i ([snk snake?])
+             [result (snk)
+                     (let ([h (car (snake-segs snk))]
+                           [segs (cdr (snake-segs snk))])
+                       (if (empty? segs)
+                           #f
+                           (memf (curry posn=? h) segs)))])]
+   [types (snake? . -> . boolean?)])
+
   (segs-self-collide? (car (snake-segs snk))
                       (cdr (snake-segs snk))))
 
 ;; segs-self-collide? : Posn Segs -> Boolean
-(define (segs-self-collide? h segs)
+(define/contract (segs-self-collide? h segs)
+  (configurable-ctc
+   [max (->i ([h posn?]
+              [segs snake-segs?])
+             [result (h segs)
+                     (if (empty? segs)
+                         #f
+                         (memf (curry posn=? h) segs))])]
+   [types (posn? snake-segs? . -> . boolean?)])
+
   (cond [(empty? segs) #f]
         [else (or (posn=? (car segs) h)
                   (segs-self-collide? h (cdr segs)))]))
