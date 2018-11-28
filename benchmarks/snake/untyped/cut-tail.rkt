ff1c872e025dd0c8fb3f9cd86b824b050c16e083
Lukas Lazarek <LukasALazarek@gmail.com>
2018-11-28 13:17:57 -0600

    snake: cut-tail: add ctcs

--- benchmarks/snake/untyped/cut-tail.rkt
+++ benchmarks/snake/untyped/cut-tail.rkt
@@ -1,13 +1,23 @@
 #lang racket
 
-(require "data.rkt")
+(require "data.rkt"
+         "../../../ctcs/precision-config.rkt"
+         "../../../ctcs/common.rkt")
 ;; NeSegs is one of:
 ;; - (cons Posn empty)
 ;; - (cons Posn NeSegs)
 
+(define ne-segs? (and/c snake-segs? cons?))
+
 ;; cut-tail : NeSegs -> Segs
 ;; Cut off the tail.
-(define (cut-tail segs)
+(define/contract (cut-tail segs)
+  (configurable-ctc
+   [max (->i ([segs ne-segs?])
+             [result (segs)
+                     (equal?/c (drop-right segs 1))])]
+   [types (ne-segs? . -> . snake-segs?)])
+
   (let ([r (cdr segs)])
     (cond [(empty? r) empty]
           [else (cons (car segs) (cut-tail r))])))
