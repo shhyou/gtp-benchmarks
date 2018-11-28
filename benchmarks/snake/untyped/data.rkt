45f58467d22da4a9449ffd952bddcd3ed6537d83
Lukas Lazarek <LukasALazarek@gmail.com>
2018-11-28 13:04:39 -0600

    snake: data: refine types and more specific contracts

--- benchmarks/snake/untyped/data.rkt
+++ benchmarks/snake/untyped/data.rkt
@@ -3,11 +3,41 @@
 (require "../../../ctcs/precision-config.rkt"
          "../../../ctcs/common.rkt")
 
-(struct snake (dir segs))
-(struct world (snake food))
+
 (struct posn (x y))
+(define food? (listof posn?))
 
 (define snake-segs? (listof posn?))
+(define snake-dir? (or/c "up"
+                         "down"
+                         "left"
+                         "right"))
+(struct snake (dir segs))
+
+(define (snake-type? s)
+  (match s
+    [(snake (? string?)
+            (? snake-segs?)) #t]
+    [_ #f]))
+
+(define ((snake/c dir/c segs/c) s)
+  (match s
+    [(snake (? (and/c snake-dir?
+                      dir/c))
+            (? (and/c snake-segs?
+                      segs/c)))
+     #t]
+    [_ #f]))
+
+(struct world (snake food))
+
+(define ((world/c snake/c food/c) x)
+  (match x
+    [(world (? snake/c) (? food/c)) #t]
+    [_ #f]))
+
+(define world-type? (world/c snake-type? food?))
+
 
 (define ((posn=?/c p1) p2)
   (and (= (posn-x p1) (posn-x p2))
@@ -34,4 +64,9 @@
  [struct-out world]
  posn?
  snake-segs?
- posn=?/c)
+ snake-dir?
+ snake-type?
+ snake/c
+ posn=?/c
+ world/c
+ world-type?)
