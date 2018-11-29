8cafe5b38208fb9f48788791ccfcfa4954b1667a
Lukas Lazarek <LukasALazarek@gmail.com>
2018-11-28 19:15:39 -0600

    snake: motion: ctcs

--- benchmarks/snake/untyped/data.rkt
+++ benchmarks/snake/untyped/data.rkt
@@ -5,11 +5,21 @@
 
 
 (struct posn (x y))
-(define food? (listof posn?))
+(define ((posn=?/c p1) p2)
+  (and (= (posn-x p1) (posn-x p2))
+       (= (posn-y p1) (posn-y p2))))
+(define ((posn/c x/c y/c) x)
+  (match x
+    [(posn (? x/c) (? y/c)) #t]
+    [_ #f]))
 
 (define snake-segs? (listof posn?))
 (define (snake-segs=?/c segs)
   (apply list/c (map posn=?/c segs)))
+
+;; lltodo: I think this is wrong, should just be one
+(define food? posn?)
+(define food=?/c posn=?/c)
 
 (define snake-dir? (or/c "up"
                          "down"
@@ -32,6 +42,14 @@
      #t]
     [_ #f]))
 
+(define ((snake=?/c s1) s2)
+  ;; lltodo: this doesn't work
+  (match* (s1 s2)
+    [((snake dir segs1)
+      (snake dir segs2))
+     ((snake-segs=?/c segs1) segs2)]
+    [(_ _) #f]))
+
 (struct world (snake food))
 
 (define ((world/c snake/c food/c) x)
@@ -40,14 +58,14 @@
     [_ #f]))
 
 (define ((world=?/c w1) w2)
-  (equal? w1 w2))
+  (match* (w1 w2)
+    [((world snake1 food1)
+      (world snake2 food2))
+     (and ((snake=?/c snake1) snake2)
+          ((food=?/c food1) food2))]
+    [(_ _) #f]))
 
 (define world-type? (world/c snake-type? food?))
-
-
-(define ((posn=?/c p1) p2)
-  (and (= (posn-x p1) (posn-x p2))
-       (= (posn-y p1) (posn-y p2))))
 
 (define/contract (posn=? p1 p2)
   (configurable-ctc
@@ -69,12 +87,15 @@
  [struct-out snake]
  [struct-out world]
  posn?
+ posn/c
  snake-segs?
  snake-segs=?/c
  snake-dir?
  snake-type?
  snake/c
+ snake=?/c
  posn=?/c
  world/c
  world-type?
- world=?/c)
+ world=?/c
+ food=?/c)
--- benchmarks/snake/untyped/motion.rkt
+++ benchmarks/snake/untyped/motion.rkt
@@ -1,38 +1,112 @@
 #lang racket  
 (require "data.rkt"
          "const.rkt"
-         "motion-help.rkt")
+         "motion-help.rkt"
+         "../../../ctcs/precision-config.rkt"
+         "../../../ctcs/common.rkt")
 
 (provide reset!)
-(define r (make-pseudo-random-generator)) 
-(define (reset!)
+(define/contract r pseudo-random-generator?
+  (make-pseudo-random-generator))
+(define/contract (reset!)
+  (configurable-ctc
+   [max (->* ()
+             void?
+             #:post
+             (equal?
+              (pseudo-random-generator->vector r)
+              (let ([r* (make-pseudo-random-generator)])
+                (parameterize ([current-pseudo-random-generator r*])
+                  (random-seed 1324)
+                  (pseudo-random-generator->vector r*)))))]
+   [types (-> void?)])
+
   (parameterize ((current-pseudo-random-generator r))
     (random-seed 1324)))
 
 ;; world->world : World -> World
-(define (world->world w)
+(define/contract (world->world w)
+  (configurable-ctc
+   [max (->i ([w world-type?])
+             [result
+              (w)
+              (match w
+                [(world (snake dir (cons h t)) food) #:when (posn=? h food)
+                 (world/c (snake/c dir
+                                   (compose (=/c (+ (length t) 2))
+                                            length))
+                          any/c)]
+                [(world (snake dir segs) food)
+                 (world/c (snake/c dir
+                                   (compose (=/c (length segs))
+                                            length))
+                          (food=?/c food))])])]
+   [types (world-type? . -> . world-type?)])
+
   (cond [(eating? w) (snake-eat w)]
         [else
          (world (snake-slither (world-snake w))
                 (world-food w))]))
 ;; eating? : World -> Boolean
 ;; Is the snake eating the food in the world.
-(define (eating? w)
+(define/contract (eating? w)
+  (configurable-ctc
+   [max (->i ([w world-type?])
+             [result (w)
+                     (match w
+                       [(world (snake _ (cons p1 _)) p2) #:when (posn=? p1 p2)
+                                               #t]
+                       [_ #f])])]
+   [types (world-type? . -> . boolean?)])
+
   (posn=? (world-food w)
           (car (snake-segs (world-snake w)))))
 ;; snake-change-direction : Snake Direction -> Snake
 ;; Change the direction of the snake.
-(define (snake-change-direction snk dir)
+(define/contract (snake-change-direction snk dir)
+  (configurable-ctc
+   [max (->i ([snk snake-type?]
+              [dir snake-dir?])
+             [result (snk dir)
+                     (snake/c dir (snake-segs snk))])]
+   [types (snake-type? string? . -> . snake-type?)])
+
   (snake dir
          (snake-segs snk)))
 ;; world-change-dir : World Direction -> World
 ;; Change direction of the world.
-(define (world-change-dir w dir)
+(define/contract (world-change-dir w dir)
+  (configurable-ctc
+   [max (->i ([w world-type?]
+              [dir snake-dir?])
+             [result (w dir)
+                     (match w
+                       [(world (snake _ segs) food)
+                        (world/c (snake/c dir (snake-segs=?/c segs))
+                                 (food=?/c food))])])]
+   [types (world-type? string? . -> . world-type?)])
+
   (world (snake-change-direction (world-snake w) dir)
          (world-food w)))
+
+
+(define-syntax-rule (save-random-excursion generator body ...)
+  (let ([generator-state (pseudo-random-generator->vector generator)])
+    (begin0
+        (begin body ...)
+      (vector->pseudo-random-generator! generator generator-state))))
+
 ;; snake-eat : World -> World
 ;; Eat the food and generate a new one.
-(define (snake-eat w)
+(define/contract (snake-eat w)
+  (configurable-ctc
+   [max (->i ([w world-type?])
+             [result (w)
+                     (world/c (snake=?/c (snake-grow (world-snake w)))
+                              (posn/c (integer-in 0 (sub1 BOARD-WIDTH))
+                                      (integer-in 0 (sub1 BOARD-HEIGHT))))])]
+   [types (world-type? . -> . world-type?)])
+
   (define i (add1 (random (sub1 BOARD-WIDTH) r)))
   (define j (add1 (random (sub1 BOARD-HEIGHT) r)))
   (world (snake-grow (world-snake w))
