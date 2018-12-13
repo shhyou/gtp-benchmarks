ffdfa87bd7b39ae5ab553815affba8e2bf6c0e0e
shhyou <shu-hung.you@eecs.northwestern.edu>
2018-12-13 13:40:19 -0600

    Add types-like contract for sieve

--- benchmarks/sieve/untyped/main.rkt
+++ benchmarks/sieve/untyped/main.rkt
@@ -1,31 +1,47 @@
 #lang racket/base
 
-(require "streams.rkt")
+(require racket/contract
+         "../../../ctcs/precision-config.rkt"
+         "streams.rkt")
+
+(provide count-from
+         sift
+         sieve
+         main)
 
 ;;--------------------------------------------------------------------------------------------------
 
 ;; `count-from n` Build a stream of integers starting from `n` and iteratively adding 1
-(define (count-from n)
+(define/contract (count-from n)
+  (configurable-ctc
+   [types (-> number? stream?)]
+   [max (-> number? stream?)])
   (make-stream n (lambda () (count-from (add1 n)))))
 
 ;; `sift n st` Filter all elements in `st` that are equal to `n`.
 ;; Return a new stream.
-(define (sift n st)
+(define/contract (sift n st)
+  (configurable-ctc
+   [types (-> integer? stream? stream?)]
+   [max (-> integer? stream? stream?)])
   (define-values (hd tl) (stream-unfold st))
   (cond [(= 0 (modulo hd n)) (sift n tl)]
         [else (make-stream hd (lambda () (sift n tl)))]))
 
 ;; `sieve st` Sieve of Eratosthenes
-(define (sieve st)
+(define/contract (sieve st)
+  (configurable-ctc
+   [types (-> stream? stream?)]
+   [max (-> stream? stream?)])
   (define-values (hd tl) (stream-unfold st))
   (make-stream hd (lambda () (sieve (sift hd tl)))))
 
-;; stream of prime numbers
-(define primes (sieve (count-from 2)))
-
-(define N-1 6666)
-
-(define (main)
+(define/contract (main N-1)
+  (configurable-ctc
+   [types (-> exact-nonnegative-integer? exact-nonnegative-integer?)]
+   [max (-> exact-nonnegative-integer? exact-nonnegative-integer?)])
+  ;; stream of prime numbers
+  (define primes (sieve (count-from 2)))
   (void (stream-get primes N-1)))
 
-(time (main))
+(time (main 6666))
--- benchmarks/sieve/untyped/streams.rkt
+++ benchmarks/sieve/untyped/streams.rkt
@@ -1,5 +1,8 @@
 #lang racket/base
 
+(require racket/contract
+         "../../../ctcs/precision-config.rkt")
+
 ;; Simple streams library.
 ;; For building and using infinite lists.
 
@@ -14,21 +17,33 @@
 
 ;;--------------------------------------------------------------------------------------------------
 
-(define (make-stream hd thunk)
+(define/contract (make-stream hd thunk)
+  (configurable-ctc
+   [types (-> any/c (-> stream?) stream?)]
+   [max (-> any/c (-> stream?) stream?)])
   (stream hd thunk))
 
 ;; `stream-unfold st` Destruct a stream `st` into its first value and the new stream produced by de-thunking the tail
-(define (stream-unfold st)
+(define/contract (stream-unfold st)
+  (configurable-ctc
+   [types (-> stream? (values any/c stream?))]
+   [max (-> stream? (values any/c stream?))])
   (values (stream-first st) ((stream-rest st))))
 
 ;; `stream-get st i` Get the `i`-th element from the stream `st`
-(define (stream-get st i)
+(define/contract (stream-get st i)
+  (configurable-ctc
+   [types (-> stream? exact-nonnegative-integer? any/c)]
+   [max (-> stream? exact-nonnegative-integer? any/c)])
   (define-values (hd tl) (stream-unfold st))
   (cond [(= i 0) hd]
         [else    (stream-get tl (sub1 i))]))
 
 ;; `stream-take st n` Collect the first `n` elements of the stream `st`.
-(define (stream-take st n)
+(define/contract (stream-take st n)
+  (configurable-ctc
+   [types (-> stream? exact-nonnegative-integer? (listof any/c))]
+   [max (-> stream? exact-nonnegative-integer? (listof any/c))])
   (cond [(= n 0) '()]
         [else (define-values (hd tl) (stream-unfold st))
               (cons hd (stream-take tl (sub1 n)))]))
